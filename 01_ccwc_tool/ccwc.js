a813cadd4aaf8ece85f6b62398825d6c736cdf3e
nona <nona@Lazyans-MacBook-Pro.local>
2024-01-07 17:32:21 +0400

    Update function names

--- 01_ccwc_tool/ccwc.js
+++ 01_ccwc_tool/ccwc.js
@@ -1,23 +1,21 @@
 #! /usr/local/bin/node
 
-function w(txt) {
-    txt = txt.toString();
-    return txt.trim().split(/\s+/).length;
-}
-function m(txt) {
-    txt = txt.toString();
-    const letters = txt.length;
-    return letters;
-}
-function c(txt) {
+function countBytes(txt) {
     const byteSize = str => new Blob([str]).size;
     return byteSize(txt);
 }
-function l(txt) {
+function countLines(txt) {
     txt = txt.toString();
     var lines = txt.split('\n');
-    var lines_count = lines.length - 1;
-    return lines_count;
+    return (lines.length - 1);
+}
+function countWords(txt) {
+    txt = txt.toString();
+    return txt.trim().split(/\s+/).length;
+}
+function countCharacters(txt) {
+    txt = txt.toString();
+    return txt.length;
 }
 
 const fs = require('fs');
@@ -51,51 +49,51 @@ try {
     if (option === '-c') {
         if(file) {
             fileContent = fs.readFileSync(file)
-            console.log(c(fileContent), file);
+            console.log(countBytes(fileContent), file);
             process.exit(0);
         } else {
             process.stdin.on('end', _ => {
-                console.log(c(stdInput));
+                console.log(countBytes(stdInput));
             });
         }
     } else if(option === '-l') {
         if(file) {
             fileContent = fs.readFileSync(file);
-            console.log(l(fileContent), file);
+            console.log(countLines(fileContent), file);
             process.exit(0);
         } else {
             process.stdin.on('end', _ => {
-                console.log(l(stdInput));
+                console.log(countLines(stdInput));
             });
         }
     } else if(option === '-w') {
         if(file) {
             fileContent = fs.readFileSync(file)
-            console.log(w(fileContent), file);
+            console.log(countWords(fileContent), file);
             process.exit(0);
         } else {
             process.stdin.on('end', _ => {
-                console.log(w(stdInput));
+                console.log(countWords(stdInput));
             });
         }
     } else if(option === '-m') {
         if(file) {
             fileContent = fs.readFileSync(file)
-            console.log(m(fileContent), file);
+            console.log(countCharacters(fileContent), file);
             process.exit(0);
         } else {
             process.stdin.on('end', _ => {
-                console.log(m(stdInput));
+                console.log(countCharacters(stdInput));
             });
         }
     } else {
         if(file) {
 	        fileContent = fs.readFileSync(file)
-            console.log(l(fileContent), w(fileContent), c(fileContent), file)
+            console.log(countLines(fileContent), countWords(fileContent), countBytes(fileContent), file)
             process.exit(0);
         } else {
             process.stdin.on('end', _ => {
-                console.log(l(stdInput), w(stdInput), c(stdInput));
+                console.log(countLines(stdInput), countWords(stdInput), countBytes(stdInput));
             }); 
         }
     }
