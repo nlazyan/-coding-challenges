cccce9a97fe49822d8fde85e42e379ba95caec73
nona <nona@Lazyans-MacBook-Pro.local>
2024-01-09 21:57:06 +0400

    Step2 solution

--- 02_json_parser/jsonParser.js
+++ 02_json_parser/jsonParser.js
@@ -5,13 +5,36 @@ var fileContent = "";
 
 if(fileName) {
     fileContent = fs.readFileSync(fileName).toString();
-    let firstChar = fileContent.charAt(0); 
-    let lastChar = fileContent.charAt(fileContent.length - 1); 
+    parseSimpleJSON(fileContent);
+}
+
+function parseSimpleJSON(fileContent) {
+    fileContent = fileContent.replaceAll(/\s/g,'');
+    let firstChar = fileContent.charAt(0);
+    let lastChar = fileContent.charAt(fileContent.length - 1);
+    console.log(firstChar, lastChar)
     if(firstChar === '{' && lastChar === '}') {
-        console.log("Valid JSON file")
+        console.log("Valid JSON")
+        let jsonContent = fileContent.slice(1, fileContent.length - 1)
+        parseStringKeyValue(jsonContent);
         process.exit(0);
-    } else {
-        console.log("Invalid JSON file")
+    }else {
+        console.log("Invalid JSON")
         process.exit(0);
     }
+}
+
+function parseStringKeyValue(jsonContent) {
+    let items = jsonContent.split(':')
+    for(item of items) {
+        let firstChar = item.charAt(0);
+        let lastChar = item.charAt(item.length - 1);
+        if(firstChar === '"' && lastChar === '"') {
+            continue;
+        } else {
+            console.log("Invalid JSON around: ", item);
+            return;
+        }
+    }
+    console.log("Valid JSON object containing string key and value")
 } 
